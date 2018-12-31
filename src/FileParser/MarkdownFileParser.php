46b02ad6c3b99f8bc8b0d819e9756c3dbfb20a90
Shane Logsdon <shane@shanelogsdon.com>
2018-12-31 17:22:32 -0500

    allow md files to use layouts

--- src/FileParser/MarkdownFileParser.php
+++ src/FileParser/MarkdownFileParser.php
@@ -6,13 +6,21 @@ use function FlatFile\Functions\markdown;
 
 class MarkdownFileParser extends FileParserAbstract
 {
-    public function parse(\SplFileInfo $file, \League\Plates\Engine $plates = null): ParsedFile
+    public function parse(\SplFileInfo $file, \League\Plates\Engine $plates = null): \stdClass
     {
         $contents = file_get_contents($file->getPathName());
         if (false === $contents) {
-            return new ParsedFile;
+            return (object)[
+                'content' => '',
+                'meta' => null,
+            ];
         }
-        return markdown($contents);
+        $result = markdown($contents);
+        if (isset($result->meta['layout'])) {
+            $template = $plates->make($result->meta['layout']);
+            $result->content = $template->render($result->meta + ['content' => $result->content]);
+        }
+        return $result;
     }
 
     public function supportedFileExtensions(): array
