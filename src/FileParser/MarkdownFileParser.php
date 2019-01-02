dde24d90535c50aa4fecdb2be03061f2eb2cccbc
Shane Logsdon <shane@shanelogsdon.com>
2019-01-01 22:11:03 -0500

    correct MarkdownFileParser

--- src/FileParser/MarkdownFileParser.php
+++ src/FileParser/MarkdownFileParser.php
@@ -6,17 +6,14 @@ use function FlatFile\Functions\markdown;
 
 class MarkdownFileParser extends FileParserAbstract
 {
-    public function parse(\SplFileInfo $file, \League\Plates\Engine $plates = null): \stdClass
+    public function parse(\SplFileInfo $file, \League\Plates\Engine $plates = null): ParsedFile
     {
         $contents = file_get_contents($file->getPathName());
         if (false === $contents) {
-            return (object)[
-                'content' => '',
-                'meta' => null,
-            ];
+            return new ParsedFile;
         }
         $result = markdown($contents);
-        if (isset($result->meta['layout'])) {
+        if (isset($result->meta['layout']) && $plates !== null) {
             $template = $plates->make($result->meta['layout']);
             $result->content = $template->render($result->meta + ['content' => $result->content]);
         }
