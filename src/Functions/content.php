b7a88ff7ef1378db6d057d7d873b55c6f9a4b243
Shane Logsdon <shane@shanelogsdon.com>
2024-12-04 15:18:22 -0500

    re-add server-side hljs

--- src/Functions/content.php
+++ src/Functions/content.php
@@ -2,16 +2,26 @@
 
 namespace FlatFile\Functions;
 
-use League\CommonMark\GithubFlavoredMarkdownConverter;
+use League\CommonMark\MarkdownConverter;
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\CommonMark\Node\Block\FencedCode;
+use League\CommonMark\Extension\CommonMark\Node\Block\IndentedCode;
+use Spatie\CommonMarkHighlighter\FencedCodeRenderer;
+use Spatie\CommonMarkHighlighter\IndentedCodeRenderer;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 use FlatFile\FileParser\ParsedFile;
 
 function markdown(string $markdown): ParsedFile
 {
-    /** @var GithubFlavoredMarkdownConverter|null $converter */
+    /** @var MarkdownConverter|null $converter */
     static $converter;
     if (!$converter) {
-        $converter = new GithubFlavoredMarkdownConverter([]);
+        $environment = new Environment([]);
+        $environment->addRenderer(FencedCode::class, new FencedCodeRenderer([]));
+        $environment->addRenderer(IndentedCode::class, new IndentedCodeRenderer([]));
+        $environment->addExtension(new CommonMarkCoreExtension());
+        $converter = new MarkdownConverter($environment);
     }
 
     $content = YamlFrontMatter::parse($markdown);
